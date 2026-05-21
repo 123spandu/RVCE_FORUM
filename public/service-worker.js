6923acb0d5a9c618c64d1c3a991288a121a3ee04
SumanthBHegde <sumanthhegde002@gmail.com>
2026-05-21 23:08:59 +0530

    fix: bump service worker cache to v2 to fix login.js caching bug

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js - Campus Connect PWA cache
-const STATIC_CACHE = 'cc-static-v1';
-const RUNTIME_CACHE = 'cc-runtime-v1';
-const POSTS_CACHE = 'cc-posts-v1';
+const STATIC_CACHE = 'cc-static-v2';
+const RUNTIME_CACHE = 'cc-runtime-v2';
+const POSTS_CACHE = 'cc-posts-v2';
 
 const STATIC_ASSETS = [
   '/',
